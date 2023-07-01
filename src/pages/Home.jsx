c9723bae204169e550f2c1cc03a8059d052f5ef7
paulodasilva <pauloluguenda0@gmail.com>
2023-07-01 14:22:54 +0100

    Enviando projeto para deploy

---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react"
+import MovieCard from "../components/MovieCard"
+
+import "./MoviesGrid.css"
+
+const moviesURL = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
+
+export const Home = () => {
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    console.log(data)
+    setTopMovies(data.results)
+  }
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`
+
+    getTopRatedMovies(topRatedUrl)
+  }, [])
+
+  return (
+    <div className="container">
+      <h2 className="title">Melhores filmes</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+  )
+}
+
+// export default Home;
